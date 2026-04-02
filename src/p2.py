71d8a6e05b43670aedafc5b7689e37f5dc3ee875
limGT <limguanting5@gmail.com>
2026-04-02 18:22:46 +0800

    Add Problem 2 solution and tests

---
+++ src/p2.py
@@ -0,0 +1,12 @@
+def fizz_buzz(array):
+    result = []
+    for i in array:
+        if i % 15 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(i)
+    return result
---
+++ src/p2_test.py
@@ -0,0 +1,4 @@
+from p2 import fizz_buzz
+
+numbers = list(range(1, 100))
+print(fizz_buzz(numbers))
